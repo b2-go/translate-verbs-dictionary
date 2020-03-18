d739021c705ef199238bc6765d5e9c0887da05fb
Baptiste Boussemart <13852255+reagere@users.noreply.github.com>
2020-03-18 08:27:22 +0100

    concurrent tx table builder

--- cmd/build_translate_table/main.go
+++ cmd/build_translate_table/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"sync"
 
 	translator "github.com/reagere/translate-verbs-dictionary"
+
+	"golang.org/x/sync/errgroup"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -25,14 +30,17 @@ func main() {
 		log.Fatal(err)
 	}
 	defer errs.Close()
+	var errsLock sync.Mutex
 
 	out, err := os.Create("output.log")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close()
+	var outLock sync.Mutex
 
 	csvout := csv.NewWriter(out)
+	defer csvout.Flush()
 	err = csvout.Write([]string{
 		"langue origine",
 		"verbe conjugue origine",
@@ -50,24 +58,78 @@ func main() {
 		log.Fatal(err)
 	}
 
-	count := 0
-	countError := 0
-	for _, lang := range os.Args[1:] {
-		list, err := translator.List(db, lang)
-		if err != nil {
-			log.Fatal(err)
+	g, ctx := errgroup.WithContext(context.Background())
+
+	_ = ctx
+	for i := range os.Args[1:] {
+		lang := os.Args[1:][i]
+		translateLang := "francais"
+		if lang == "francais" {
+			translateLang = "quechua"
 		}
-		// id, verb_inf, verb, itr, tr, langues, flex, flexOpts, pers, plur, form
-		for i := range list {
-			verb := list[i][2].(string)
-			translateLang := "francais"
-			if lang == "francais" {
-				translateLang = "quechua"
+		g.Go(func() error {
+			list, err := translator.List(db, lang)
+			if err != nil {
+				return err
 			}
-			translation, err := tx.Translate(lang, verb, translateLang)
-			count++
+
+			b := &Builder{
+				lang:          lang,
+				translateLang: translateLang,
+				errsLock:      &errsLock,
+				errs:          errs,
+				outLock:       &outLock,
+				csvout:        csvout,
+				tx:            tx,
+				list:          list,
+			}
+
+			size := len(list)
+			splitSize := size / 5
+			offset := 0
+			for split := 0; split < 4; split++ {
+				g.Go(b.run(offset, offset+splitSize))
+				offset += split
+			}
+			g.Go(b.run(offset, size))
+			return nil
+		})
+	}
+	err = g.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Builder struct {
+	lang          string
+	translateLang string
+	tx            *translator.Service
+	errsLock      *sync.Mutex
+	errs          io.Writer
+	outLock       *sync.Mutex
+	csvout        *csv.Writer
+	list          [][]interface{}
+}
+
+func (b *Builder) run(offset, limit int) func() error {
+	return func() error {
+		// id, verb_inf, verb, itr, tr, langues, flex, flexOpts, pers, plur, form
+		for i := offset; i < limit; i++ {
+			verb, ok := b.list[i][2].(string)
+			if !ok {
+				b.errsLock.Lock()
+				_, err := b.errs.Write([]byte("not a string"))
+				b.errsLock.Unlock()
+				if err != nil {
+					return err
+				}
+				continue
+			}
+			translation, err := b.tx.Translate(b.lang, verb, b.translateLang)
 			if err == nil {
-				err = csvout.Write([]string{
+				b.outLock.Lock()
+				err = b.csvout.Write([]string{
 					translation.LangueOrigine,
 					translation.VerbeConjugueOrigine,
 					strconv.Itoa(translation.PersonneOrigine),
@@ -80,16 +142,19 @@ func main() {
 					strconv.Itoa(translation.PlurielTraduit),
 					translation.FormeTraduit,
 				})
+				b.outLock.Unlock()
 				if err != nil {
-					log.Fatal(err)
+					return err
 				}
 			} else {
-				countError++
-				errs.Write([]byte(fmt.Sprintf("%s %s -> %v\n", lang, verb, err)))
-			}
-			if count%100 == 0 {
-				log.Printf("err : %d ; count : %d", countError, count)
+				b.errsLock.Lock()
+				_, err = b.errs.Write([]byte(fmt.Sprintf("%s %s -> %v\n", b.lang, verb, err)))
+				b.errsLock.Unlock()
+				if err != nil {
+					return err
+				}
 			}
 		}
+		return nil
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/reagere/translate-verbs-dictionary
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v2.0.1+incompatible
+require (
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
+)
